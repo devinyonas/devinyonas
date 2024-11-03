66f15d8e40d3f991f670a285ecb9f60b8783a267
devinyonas <devin.yonas@gmail.com>
2024-11-04 00:02:18 +0700

    init landing page

---
+++ landing_page/lib/generated/fonts.gen.dart
@@ -0,0 +1,21 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+class FontFamily {
+  FontFamily._();
+
+  /// Font family: jacquard
+  static const String jacquard = 'jacquard';
+
+  /// Font family: noto
+  static const String noto = 'noto';
+
+  /// Font family: pixelify
+  static const String pixelify = 'pixelify';
+}
